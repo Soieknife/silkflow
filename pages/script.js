6d344e3bcdeac726536484bde92d3c42651543fc
“pingan” <1901382573@qq.com>
2026-07-30 22:54:43 +0800

    chore: 删除 GitHub Pages 部署相关文件和页面内容

--- pages/script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.site-header')
-
-function syncHeaderState() {
-  if (!header) return
-  header.toggleAttribute('data-scrolled', window.scrollY > 8)
-}
-
-syncHeaderState()
-window.addEventListener('scroll', syncHeaderState, { passive: true })
